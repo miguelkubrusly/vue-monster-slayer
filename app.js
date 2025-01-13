55b8ab0d10a48915d71939d5ba8a1d852bdace3a
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2025-01-12 22:27:57 -0300

    feat(app): ✨ make all methods and computed props
    add winner var to data

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-const generateRandomNum = (min, max) => {};
+const generateRandomNum = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const M = "monster";
+const P = "player";
+const D = "draw";
 
 const app = Vue.createApp({
   data() {
@@ -7,23 +13,61 @@ const app = Vue.createApp({
       monsterLife: 100,
       currentRound: 0,
       logMessages: [],
+      winner: null,
     };
   },
   methods: {
-    playerAttack() {},
-    playerHeal() {},
-    playerSpecialAttack() {},
-    playerSurrender() {},
-    monsterAttack() {},
-    createLogMessage() {},
-    restartGame() {},
+    playerAttack() {
+      const attackValue = generateRandomNum(12, 5);
+      this.monsterLife -= attackValue;
+      this.monsterAttack();
+      this.createLogMessage(P.toUpperCase(), "attack", attackValue);
+    },
+    playerHeal() {
+      const healingValue = generateRandomNum(15, 3);
+      this.playerLife += healingValue;
+      this.createLogMessage(P.toUpperCase(), "heal", healingValue);
+    },
+    playerSpecialAttack() {
+      const specialAttackValue = generateRandomNum(22, 12);
+      this.monsterLife -= specialAttackValue;
+      this.monsterAttack();
+      this.createLogMessage(
+        P.toUpperCase(),
+        "special attack",
+        specialAttackValue
+      );
+    },
+    playerSurrender() {
+      this.winner = M;
+    },
+    monsterAttack() {
+      const attackValue = generateRandomNum(18, 10);
+      this.playerLife -= attackValue;
+      this.monsterLife -= attackValue;
+      this.createLogMessage(M.toUpperCase(), "attack", attackValue);
+    },
+    createLogMessage(who, what, value) {
+      const message = {
+        who,
+        what,
+        value,
+      };
+    },
+    restartGame() {
+      this.playerLife = 100;
+      this.monsterLife = 100;
+      this.currentRound = 0;
+      this.logMessages = [];
+      this.winner = null;
+    },
   },
   computed: {
     playerLifeBarStyle() {
-      return {};
+      return { width: this.playerLife };
     },
     monsterLifeBarStyle() {
-      return {};
+      return { width: this.monsterLife };
     },
   },
   watchers: {
